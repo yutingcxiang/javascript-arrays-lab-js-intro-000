8a9a07b1a04165d8b0499926312bb574f47a244f
Christine Xiang <yutingcxiang@gmail.com>
2018-06-15 00:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,14 @@ function prependKitten(name) {
   var newArray = [name, ...kittens];
   return newArray;
 }
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length-1);
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1);
+  return newArray;
+}
+
